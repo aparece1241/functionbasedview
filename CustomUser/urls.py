a8eef21dc61166db49252048c6897bd17197187a
aparece1241 <apareceh7@gmail.com>
2021-03-28 21:30:34 +0800

    [UPDATE] update the setting.py change drf_token to jwt

--- CustomUser/urls.py
+++ CustomUser/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('users', views.get_all_user, name='users'),
     path('add', views.add_user, name='add_user'),
+    path('login', jwt_views.TokenObtainPairView.as_view(), name='login'),
+    path('refresh', jwt_views.TokenRefreshView.as_view(), name='refresh_token')
 ]
